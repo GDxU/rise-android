131cdf1ed3259763e07fac98b05fa28a89b0889a
Matthew Tole <matthewtole@gmail.com>
2012-08-29 16:11:06 +0100

    Remove redundance worker disc piece
    The WorkerDisc class was entirely redundant, so I removed it.

--- src/com/matthewtole/androidrise/game/pieces/Worker.java
+++ src/com/matthewtole/androidrise/game/pieces/Worker.java
@@ -3,6 +3,7 @@ package com.matthewtole.androidrise.game.pieces;
 import com.matthewtole.androidrise.game.SpriteManager;
 import com.matthewtole.androidrise.game.enums.GamePlayer;
 import com.matthewtole.androidrise.lib.PieceCallback;
+import com.matthewtole.androidrise.lib.Utils;
 
 public class Worker extends CompositePiece implements PieceCallback {
 
@@ -13,7 +14,7 @@ public class Worker extends CompositePiece implements PieceCallback {
 		this.player = player;
 
 		this.addPiece(new AnimatedPiece(sprites, "highlight2", 4, 2));
-		this.addPiece(new WorkerDisc(sprites, player));		
+		this.addPiece(new BitmapPiece(sprites, Utils.playerString(player) + "_worker"));		
 		this.hidePiece(0);
 		((AnimatedPiece)this.getPiece(0)).setCallback(this);
 		((AnimatedPiece)this.getPiece(0)).pause();		
